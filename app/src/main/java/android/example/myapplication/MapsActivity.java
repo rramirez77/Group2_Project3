f314412527b8d19d12eee1dc158ef7630de85b2b
estrk467 <kvillaestrella@horizon.csueastbay.edu>
2022-05-03 11:50:12 -0700

    Location tracking bug fixed

--- app/src/main/java/android/example/myapplication/MapsActivity.java
+++ app/src/main/java/android/example/myapplication/MapsActivity.java
@@ -27,7 +27,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.CancellationToken;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -53,7 +55,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //binding to display map
     private ActivityMapsBinding binding;
 
-   LatLng latLng;
+    LatLng latLng;
     //LocationCallback for tracking locaiton
     private LocationCallback locationCallback;
     //Stop tracking button
@@ -66,8 +68,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //polyline
     PolylineOptions polylineOptions;
     Polyline polyline;
-
-
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -85,7 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         client = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
         //initialize datareference to user that's logged in
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-       //initialize route2 polyline
+        //initialize route2 polyline
         //route2 = new PolylineOptions();
 
 
@@ -98,13 +98,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View view) {
                 //When click
-                //startTracking
                 polylineOptions = new PolylineOptions();
 
 
                 getLocation();
                 startTracking();
-                //zoomIn();
+
                 //show user their location
                 showUserLocation();
                 startTracking.setVisibility(View.GONE);
@@ -133,13 +132,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 databaseReference.child("Journeys").child("LatLng").child("latitude").setValue(null);
                 databaseReference.child("Journeys").child("LatLng").child("longitude").setValue(null);
-                //polylineOptions = new PolylineOptions();
-                polylineOptions = new PolylineOptions();
-                //polyline = mMap.addPolyline(polylineOptions);
-                //polyline =mMap.addPolyline(polylineOptions);
 
-                //delete points on polyline
-                //
+                polylineOptions = new PolylineOptions();
+
+
+
                 stopTracking.setVisibility(View.GONE);
                 startTracking.setVisibility(View.VISIBLE);
 
@@ -173,9 +170,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //Ask user for location tracking permission
         if(ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            //if permission granted
-            //get the location
-            //getLocation();
 
 
         }else{
@@ -194,7 +188,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 for (Location location : locationResult.getLocations()) {
                     // Update UI with location data
                     // ADD to list
-                    //databaseReference.getReference("Location").child("Location").setValue("Lat: " + location.getLongitude());
+
                     LatLng tempLatLng= new LatLng(location.getLatitude(), location.getLongitude());
 
 
@@ -233,10 +227,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //show user's location
-        //getLocation();
 
-    }//END OF onMapRead
+
+    }
 
 
 
@@ -259,17 +252,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //if location is enabled
         if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-              LocationRequest locationRequest = LocationRequest.create();
-              locationRequest.setPriority(PRIORITY_HIGH_ACCURACY);
-              locationRequest.setInterval(10000);
-              client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(10000);
+            client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
 
 
-            }
+        }
 
         else{
             //ask for permission first
-            Toast.makeText(this, ":/", Toast.LENGTH_SHORT).show();
+
 
         }
 
@@ -300,205 +293,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @SuppressLint("MissingPermission")
     private void getLocation() {
 
-        client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location != null) {
-                    LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
-                    databaseReference.child("Journeys").child("LatLng").setValue(temp);
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(temp, 16f));
-                }
-            }
-        });
-
-    }
-
-    @SuppressLint("MissingPermission")
-    private void zoomIn(){
-
-        /*client.getCurrentLocation(PRIORITY_HIGH_ACCURACY, new CancellationToken() {
-            @NonNull
-            @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
-            @Override
-            public boolean isCancellationRequested() {
-                return false;
-            }
-        }).addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(temp, 16f));
-                }
-
-
-
-
-            }
-        });*/
-
-        client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
-
-                }
-
-            }
-        });
-    }
-
-
-
-
-
-
-}//END OF CLASS
-
-
-
-
-/*****OLD******/
-
-/*LatLng cupertino = new LatLng(37, -122);
-        mMap.addMarker(new MarkerOptions().position(cupertino).title("Cupertino"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cupertino, 16f));*/
-
-/*
-    ValueEventListener postListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            // Get Post object and use the values to update the UI
-            //Post post = dataSnapshot.getValue(Post.class);
-            // ..
-
-            Log.d("TAG", "Latitude " + dataSnapshot);
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            // Getting Post failed, log a message
-            Log.d("TAG", "databaseError:onCancelled", databaseError.toException());
-        }
-    };*/
-
- /*
-        databaseReference.child("Journeys").child("LatLng").child("Longitude").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                System.out.println("Longitude: " + snapshot.child("Longitude").getValue());
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                System.out.println("DATABASE LONGITUDE ERROR");
-
-            }
-        });*/
-
-/*
-DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/uuuu");
-        date = LocalDate.now();
-        String formattedDate = dtf.format(date);
-
- */
-
-/*
-        databaseReference.child("Journeys").child("LatLng").child("latitude").addValueEventListener(new ValueEventListener() {
-@Override
-public void onDataChange(@NonNull DataSnapshot snapshot) {
-        Double latitude = snapshot.getValue(Double.class);
-        Log.d("TAG", "onDataChangeLat: " + latitude );
-        snapshot.
-        }
-
-@Override
-public void onCancelled(@NonNull DatabaseError error) {
-
-        }
-        });
-
-        databaseReference.child("Journeys").child("LatLng").child("longitude").addValueEventListener(new ValueEventListener() {
-@Override
-public void onDataChange(@NonNull DataSnapshot snapshot) {
-        Double longitude = snapshot.getValue(Double.class);
-        Log.d("TAG", "onDataChangeLong: " + longitude);
-
-        }
-
-@Override
-public void onCancelled(@NonNull DatabaseError error) {
-
-        }
-        });
-        */
-
-/*
-        databaseReference.child("Journeys").child("LatLng").addChildEventListener(new ChildEventListener() {
-
-
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                Double currentValue = snapshot.getValue(Double.class);
-
-                Double latitude = snapshot.child("latitude").getValue(Double.class);
-                Double longitude = snapshot.child("longitude").getValue(Double.class);
-                //LatLng ll= new LatLng();
-               // ll = new LatLng(latitude, longitude);
-
-
-                Log.d("TAG", "onChildAdded: LAT" + latitude);
-                Log.d("TAG", "onChildAdded LNG " + longitude);
-
-
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                //double currentValue = snapshot.getValue(Double.class);
-                //Log.d("TAG", "onChildChanged: " + currentValue);
-                Double latitude = snapshot.child("latitude").getValue(Double.class);
-                Double longitude = snapshot.child("longitude").getValue(Double.class);
-                //LatLng ll= new LatLng();
-                // ll = new LatLng(latitude, longitude);
-
-
-                Log.d("TAG", "onChildAdded: LAT" + latitude);
-                Log.d("TAG", "onChildAdded LNG " + longitude);
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-
-
-        });
-
-*/
-/*
-    public void getLocation(){
-        Location newLocation = new Location(LocationManager.GPS_PROVIDER);
         client.getCurrentLocation(PRIORITY_HIGH_ACCURACY, new CancellationToken() {
             @NonNull
             @Override
@@ -513,71 +307,51 @@ public void onCancelled(@NonNull DatabaseError error) {
         }).addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+                if (location != null) {
+                    LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
+                    databaseReference.child("Journeys").child("LatLng").setValue(temp);
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(temp, 16f));
+                }
 
-                newLocation.set(location);
 
-                getCurrentLocation(newLocation);
 
 
             }
         });
-    }*/
-/*
-private void drawPoints(){
-    Location tempLocation;
-    LatLng latLng;
-    //databaseReference.child("Journeys").child("LatLng").child("Latitude").addValueEventListener(postListener);
-/*
-        databaseReference.child("Journeys").child("LatLng").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-               // Log.d("TAG", "LatitudeFromDBresults " + snapshot.getValue());
-                //System.out.println("Latitude: " + snapshot.getValue());
 
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    latlang.new
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                System.out.println("DATABASE LATITUDE ERROR");
-
-            }
-        });***********
-
-
-    if(listOfLocations.isEmpty()){
-        Toast.makeText(this, "Location list is empty", Toast.LENGTH_SHORT).show();
-
-
-    }else{
-
-        Toast.makeText(this, "ListSize: " + listOfLocations.size(), Toast.LENGTH_SHORT).show();
-        PolylineOptions route = new PolylineOptions();
-        LatLng latlng;
-
-
-
-        for(int i =0; i<listOfLocations.size(); i++){
-            latlng = new LatLng(listOfLocations.get(i).getLatitude(), listOfLocations.get(i).getLongitude());
-            route.add(latlng);
-
-        }
-
-        mMap.addPolyline(route);
-        LatLng tempLatLng = new LatLng(listOfLocations.getFirst().getLatitude(), listOfLocations.getFirst().getLongitude());
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(tempLatLng, 14f));
-
-
-            /*
-            tempLocation = listOfLocations.getFirst();
-            latLng = new LatLng(tempLocation.getLatitude(), tempLocation.getLongitude());
-
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f));*******
 
 
     }
-}*/
+
+    @SuppressLint("MissingPermission")
+    private void zoomIn(){
+
+
+
+
+        client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
+
+                }
+
+            }
+        });
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
